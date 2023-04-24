f58511688afed4042022acd3dd0a2cf945bc1b1d
Dimitrije Pavlov <dpavlov@college.harvard.edu>
2023-04-24 14:48:06 -0400

    Add http client and server

---
+++ http_client.py
@@ -0,0 +1,64 @@
+import socket
+import sys
+import json
+from client_restructured import TorClient
+from nacl.signing import VerifyKey
+from tor_protocol import *
+from random import sample
+
+request_template = 'GET / HTTP/1.0\r\nHost: {hostname}\r\n\r\n'
+
+
+def get_request(hostname: str) -> bytes:
+    request = request_template.format(hostname=hostname)
+    return bytes(request, "utf-8")
+
+
+def main(argv):
+    if len(argv) != 2 and len(argv) != 5:
+        print(
+            "usage: python3 %s <SERVER IP> [USETOR <DS PUBLIC KEY> <DS IP>]" % argv[0])
+        return 1
+    hostname = argv[1]
+    port = 80
+    if len(argv) == 5:
+        with open(argv[3], "rb") as ds_keyfile:
+            ds_publickey = ds_keyfile.read(32)
+    request = get_request(hostname)
+    if len(argv) == 2:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(4)
+        sock.connect((hostname, port))
+        print("Successfullly connected to %s:%d" % (hostname, port))
+        send_all(sock, request)
+        response = recv_all(sock, 4096)
+    else:
+        client = TorClient(argv[4], VerifyKey(ds_publickey))
+        all_ip_addrs, all_pks = client.retrieve_onion_routers()
+        ip_addrs, pks = zip(*sample(list(zip(all_ip_addrs, all_pks)), 3))
+        print('')
+        for i in range(3):
+            sk = client.create_onion_router(socket.inet_ntoa(ip_addrs[i]))
+            client.receive_created(sk, pks[i])
+            print('')
+        client.begin(hostname, port)
+        if (client.receive_connected()):
+            print("Successfullly connected to %s:%d" % (hostname, port))
+        else:
+            print("Connection to %s:%d failed" % (hostname, port))
+            return 0
+        print('')
+        client.send_data(request)
+        response = client.recv_data()
+        client.destroy()
+    response = str(response, "utf-8")
+    response_begin = response.find("<h1>") + 4
+    response_end = response.find("</h1>")
+    response = response[response_begin:response_end]
+    print('')
+    print("Website says:", response)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
---
+++ http_server.py
@@ -0,0 +1,56 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import requests
+
+
+def response_denied(country: str) -> bytes:
+    denied_template = "<html>" \
+                      "<head><title>Access Denied</title></head>" \
+                      "<body><h1>Unfortunately, you are not allowed to access this website from {country}.</h1></body>" \
+                      "</html>"
+    if country is None:
+        denied = denied_template.format(country="your country")
+    else:
+        denied = denied_template.format(country=country)
+    return bytes(denied, "utf-8")
+
+
+def response_accepted(country: str) -> bytes:
+    accepted_template = "<html>" \
+                        "<head><title>Welcome</title></head>" \
+                        "<body><h1>Welcome to our nice website, visitor from {country}.</h1></body>" \
+                        "</html>"
+    if country is None:
+        accepted = accepted_template.format(country="your country")
+    else:
+        accepted = accepted_template.format(country=country)
+    return bytes(accepted, "utf-8")
+
+
+def get_country(ip: str) -> str:
+    data = requests.get("https://ipinfo.io/{}/json".format(ip)).json()
+    if "country" not in data:
+        return None
+    else:
+        return data["country"]
+
+
+class Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        country = get_country(self.client_address[0])
+        if country in ["US", None]:
+            response = response_denied(country)
+            print("Reqeust received from {} -- denied".format(country))
+        else:
+            response = response_accepted(country)
+            print("Reqeust received from {} -- accepted".format(country))
+        self.wfile.write(response)
+
+
+if __name__ == "__main__":
+    server = HTTPServer(('0.0.0.0', 80), Server)
+    print("Server started http://%s:%s" % ('0.0.0.0', 80))
+    server.serve_forever()
+    server.server_close()
