b2c153464953f5fa8cf2e34b1d7905b7105eec29
Dimitrije Pavlov <dpavlov@college.harvard.edu>
2023-04-24 20:32:14 -0400

    Update ChatGPT client

--- chatgpt_client.py
+++ chatgpt_client.py
@@ -24,12 +24,12 @@ def main(argv):
     port = 443
     if len(argv) != 2 and len(argv) != 5:
         print(
-            "usage: python3 %s <API KEY> [<DS PUBLIC KEY> <DS IP ADDRESS> USETOR]" % argv[0])
+            "usage: python3 %s <API KEY> [USETOR <DS PUBLIC KEY> <DS IP>]" % argv[0])
         return 1
     with open(argv[1], "r") as api_key_file:
         api_key = api_key_file.read().splitlines()[0]
     if len(argv) == 5:
-        with open(argv[2], "rb") as ds_keyfile:
+        with open(argv[3], "rb") as ds_keyfile:
             ds_publickey = ds_keyfile.read(32)
     gpt_question = input("Enter a question for ChatGPT: ")
     request = get_request(api_key, gpt_question)
@@ -38,11 +38,11 @@ def main(argv):
             socket.AF_INET, socket.SOCK_STREAM), server_hostname=hostname)
         new_sock.settimeout(4)
         new_sock.connect((hostname, port))
-        print("Successfullly connected to %s:%d" % (hostname, port))
+        print("Successfully connected to %s:%d" % (hostname, port))
         send_all(new_sock, request)
         response = recv_all(new_sock, 1048576)
     else:
-        client = TorClient(argv[3], VerifyKey(ds_publickey))
+        client = TorClient(argv[4], VerifyKey(ds_publickey))
         all_ip_addrs, all_pks = client.retrieve_onion_routers()
         ip_addrs, pks = zip(*sample(list(zip(all_ip_addrs, all_pks)), 3))
         print('')
@@ -52,7 +52,7 @@ def main(argv):
             print('')
         client.begin(hostname, port)
         if (client.receive_connected()):
-            print("Successfullly connected to %s:%d" % (hostname, port))
+            print("Successfully connected to %s:%d" % (hostname, port))
         else:
             print("Connection to %s:%d failed" % (hostname, port))
             return 0
